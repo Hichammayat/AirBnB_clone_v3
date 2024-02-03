784620b36369da293fe188477ad5ca4a19438201
hichammayat <hichammayat07@gmail.com>
2024-02-03 04:23:27 -0800

    commit

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define routes for blueprint
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """Return status of application
+    """
+    return jsonify({'status': 'OK'})
