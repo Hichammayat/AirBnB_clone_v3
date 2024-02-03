0d91dc8a8a34e63fb732ffea658d1fbe3a0ee5f4
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-02-03 07:15:12 -0800

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,14 @@ def status():
     """Return status of application
     """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats',methods=['GET'], strict_slashes=False)
+def get_stats():
+    """ retrieves the number of each objects by type
+    """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
